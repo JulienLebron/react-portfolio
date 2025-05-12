d00922d9ae884144272af52ce16d4ac45042b276
Julien Lebron <lebron-pro@outlook.fr>
2025-05-12 14:23:30 +0200

    feat: PB-0001 Create Hero Section

---
+++ src/components/FlipWords.jsx
@@ -0,0 +1,83 @@
+"use client";
+import React, { useState, useEffect, useId } from "react";
+
+import { motion as Motion } from "motion/react";
+import { twMerge } from "tailwind-merge";
+
+export function FlipWords({
+  words = ["better", "modern", "beautiful", "awesome"],
+  interval = 3000,
+  className,
+  textClassName,
+  animationDuration = 700,
+}) {
+  const id = useId();
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [width, setWidth] = useState(100);
+  const textRef = React.useRef(null);
+
+  const updateWidthForWord = () => {
+    if (textRef.current) {
+      // Add some padding to the text width (30px on each side)
+      // @ts-ignore
+      const textWidth = textRef.current.scrollWidth + 30;
+      setWidth(textWidth);
+    }
+  };
+
+  useEffect(() => {
+    // Update width whenever the word changes
+    updateWidthForWord();
+  }, [currentWordIndex]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+      // Width will be updated in the effect that depends on currentWordIndex
+    }, interval);
+
+    return () => clearInterval(intervalId);
+  }, [words, interval]);
+
+  return (
+    <Motion.p
+      layout
+      layoutId={`words-here-${id}`}
+      animate={{ width }}
+      transition={{ duration: animationDuration / 2000 }}
+      className={twMerge("z-10 relative inline-block text-left ", className)}
+      key={words[currentWordIndex]}
+    >
+      <Motion.div
+        transition={{
+          duration: animationDuration / 1000,
+          ease: "easeInOut",
+        }}
+        className={twMerge("inline-block", textClassName)}
+        ref={textRef}
+        layoutId={`word-div-${words[currentWordIndex]}-${id}`}
+      >
+        <Motion.div className="inline-block">
+          {words[currentWordIndex].split("").map((letter, index) => (
+            <Motion.span
+              key={index}
+              initial={{
+                opacity: 0,
+                filter: "blur(10px)",
+              }}
+              animate={{
+                opacity: 1,
+                filter: "blur(0px)",
+              }}
+              transition={{
+                delay: index * 0.02,
+              }}
+            >
+              {letter}
+            </Motion.span>
+          ))}
+        </Motion.div>
+      </Motion.div>
+    </Motion.p>
+  );
+}
