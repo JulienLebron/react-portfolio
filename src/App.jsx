8ac10b42129ca9bef6d4e09b1a876cbbe511c6d8
Julien Lebron <lebron-pro@outlook.fr>
2025-05-12 14:23:30 +0200

    feat: PB-0001 Create Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
+      <Hero />
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
+      <section className="min-h-screen"></section>
     </div>
   );
 };
